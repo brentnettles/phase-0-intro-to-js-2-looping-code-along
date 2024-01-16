ef5d70a031c99a1efe0b045460e864842736f8be
Brent Nettles <BrentNettles@gmail.com>
2024-01-16 15:19:50 -0500

    looping lab-writeCard

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouMessages = [];
+  
+    // use array
+    for (let i = 0; i < names.length; i++) {
+      // message
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+}
+
+// no duplicate countDown functions
+function countDown(startingNumber) {
+    let currentNumber = startingNumber;  
+
+    while (currentNumber >= 0) {
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
